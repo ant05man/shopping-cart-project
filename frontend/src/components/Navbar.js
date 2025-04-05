eda8df23e5fe25061717dc19b771d6d6fe83aa7e
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-05 19:55:21 -0400

    updated Navbar to show number of items

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = ({ cartItemCount }) => {
   return (
     <nav>
-      <h2>Shopping Cart</h2>
-      <div>Cart: 0 items</div>
+      <h2>My Shopping Cart</h2>
+      <div>Cart: {cartItemCount} items</div>
     </nav>
   );
 };
