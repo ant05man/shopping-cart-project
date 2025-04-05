ceb1990e1822bb118d11bb292d458acc8cf52df2
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-05 05:23:14 -0400

    initial navbar population

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Shopping Cart</h2>
+      <div>Cart: 0 items</div>
+    </nav>
+  );
+};
+
+export default Navbar;
