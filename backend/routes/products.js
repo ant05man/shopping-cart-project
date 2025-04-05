a0aa38052e15c02045717f4dbc0b56143eb3c77f
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-04 20:48:49 -0400

    populated products route w/ get route

---
+++ backend/routes/products.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router();
+
+// Get all products
+router.get('/products', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+module.exports = router;
