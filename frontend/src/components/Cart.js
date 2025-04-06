555f027d7844d3b3997e9388493a5fcce73bc3d7
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-05 21:00:28 -0400

    added REmove from cart button on Cart.js component

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -12,7 +12,21 @@ const Cart = ({ cart }) => {
         <ul>
           {cart.map((item, index) => (
             <li key={index}>
-              {item.name} - ${item.price.toFixed(2)}
+              {item.name} - ${item.price.toFixed(2)}{' '}
+              <button
+                onClick={() => removeFromCart(index)}
+                style={{
+                    marginLeft: '10px',
+                    padding: '4px 8px',
+                    background: '#dc3545',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: 'pointer'
+                }}
+                >
+                    Remove
+                </button>
             </li>
           ))}
         </ul>
