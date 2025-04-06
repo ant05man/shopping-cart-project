14827d2a200f45b2c36a74beb4aff20a743d4a9a
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-06 15:35:13 -0400

    added dotenv.config in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const orderRoutes = require('./routes/order');
 
 // Load environment variables
