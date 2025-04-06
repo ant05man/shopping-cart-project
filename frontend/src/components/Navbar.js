84917e3b5cbac2af01214e22cae796a6c84c20ba
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-06 11:21:45 -0400

    added links for signup and login in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ cartItemCount }) => {
   return (
     <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem' }}>
-      <Link to="/">🛍️ Shop</Link>
-      <Link to="/cart">🛒 Cart ({cartItemCount})</Link>
+    <Link to="/">🛍️ Shop</Link>
+    <Link to="/cart">🛒 Cart ({cartItemCount})</Link>
     <Link to="/checkout">✅Checkout</Link>
-    </nav>
+    <Link to="/signup">Sign Up</Link>
+    <Link to="login">Log In</Link>
+</nav>
   );
 };
 
