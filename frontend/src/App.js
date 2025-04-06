4e1294a59499bfa5b3ae911c15040bdb9116565f
Anthony Littlejohn <ant05man@yahoo.com>
2025-04-06 13:50:13 -0400

    cleand up redirect command and used navigate instead inside appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// ProtectedRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
-import PrivateRoute from './components/PrivateRoute';
-import Signup from './components/Signup';
-import Login from './components/Login';
+const ProtectedRoute = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem('user')); // Check if the user is logged in
 
-import './App.css';
+  if (!user) {
+    // Redirect to login if no user is found in localStorage
+    return <Navigate to="/login" />;
+  }
 
+  return children; // If user is logged in, render the protected route
+};
 
-function App() {
-  // Load Cart from LocalStorage or start with an empty array
-  const [cart, setCart] = useState(() => {
-  const savedCart = localStorage.getItem('cart');
-  return savedCart ? JSON.parse(savedCart) : [];
-});
-
-// whenever cart changes, save it to LocalStorage
-useEffect(() => {
-  localStorage.setItem('cart', JSON.stringify(cart));
-}, [cart]);
-
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
-  const removeFromCart = (productToRemove) => {
-    setCart(cart.filter((item, index) => index !== productToRemove));
-  };
-
-  const clearCart = () => {
-    setCart([]);
-  };
-
-  return (
-    <Router>
-      <Navbar cartItemCount={cart.length} />
-      <Routes>
-        <Route path="/" element={<ProductList addToCart={addToCart} />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/checkout" element={
-          <PrivateRoute>
-            <Checkout />
-          </PrivateRoute>
-        } 
-        />
-        <Route path="/cart" 
-        element={
-        <Cart
-         cart={cart}
-         removeFromCart={removeFromCart}
-         clearCart={clearCart}
-         />
-        }
-        />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+export default ProtectedRoute;
